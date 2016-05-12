fd57f2c66bf06ff14ec3b38cfbb11f221971d3c8
Marcel <m.akerboom@rocleiden.nl>
2016-05-12 21:38:30 +0200

    Systems check

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <title>AV Apparatuur</title>
+    </head>
+    <body>
+        <h2>Systems check</h2>
+        <dl>
+            <dt>HTML</dt>
+            <dd>OK</dd>
+            <dt>PHP</dt>
+            <dd><?php echo "OK"; ?></dd>
+            <dt>Javascript</dt>
+            <dd><script type="application/javascript">document.write("OK");</script></dd>
+        </dl>
+    </body>
+</html>
