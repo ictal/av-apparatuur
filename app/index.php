8c8a21b7128a76587efeb71cbad6b86cd8bd0544
Marcel <m.akerboom@rocleiden.nl>
2016-05-12 22:17:14 +0200

    Mysql chcek

--- app/index.php
+++ app/index.php
@@ -8,10 +8,26 @@
         <dl>
             <dt>HTML</dt>
             <dd>OK</dd>
+            <dt>Javascript</dt>
+            <dd>
+                <script type="application/javascript">document.write("OK");</script>
+            </dd>
             <dt>PHP</dt>
             <dd><?php echo "OK"; ?></dd>
-            <dt>Javascript</dt>
-            <dd><script type="application/javascript">document.write("OK");</script></dd>
+            <dt>Mysql</dt>
+            <dd>
+                <?php
+                    $connection = new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'));
+                    if($connection) {
+                        if($connection->select_db(getenv('DB_NAME'))) {
+                            echo "No DB";
+                        }
+                        echo $connection->ping() ? "OK" : $connection->error;
+                    } else {
+                        echo "No Connection";
+                    }
+                ?>
+            </dd>
         </dl>
     </body>
 </html>
